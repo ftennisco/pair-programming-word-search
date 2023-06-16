7857e74eaa0a8dedd59bf5f5041de844860f35f0
ftennisco <f.tennisco@hotmail.com>
2023-06-15 18:10:19 -0700

    Paired programming work with Blessing, fixed the wordsearch code.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    for (const l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+
+
+    const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i]).join(''));
+
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
+    return false
+};
 
 module.exports = wordSearch
